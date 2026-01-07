92d2ac2745deab8e33f3facb4eba1883dbc7cb0f
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-01-07 12:39:14 +0200

    docs: fix annotations in config.lua

--- lua/koda/config.lua
+++ lua/koda/config.lua
@@ -2,8 +2,9 @@
 local M = {}
 
 ---@class UserConfig
----@field transparent? boolean Enable background transparency (default: false)
----@field italics? boolean Enable italicized text for comments/strings (default: false)
+---@field bold? boolean Enable/disable bold text for functions/returns (default: false)
+---@field italic? boolean Enable/disable italicized text for comments/strings (default: false)
+---@field transparent? boolean Enable/disable background transparency (default: false)
 ---@field colors? table<string, string> Override palette hex codes
 
 ---@type UserConfig
