024d36e68b34b17dc55d4adf83af53a96d16f497
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-01-18 21:07:20 +0100

    feat(test): add file integrity check

--- tests/groups_spec.lua
+++ tests/groups_spec.lua
@@ -2,6 +2,21 @@ local utils = require("koda.utils")
 local palette = require("koda.palette.dark")
 local Groups = require("koda.groups")
 
+describe("File integrity:", function()
+  it("can require every file in koda/groups without syntax errors", function()
+    local path = "lua/koda/groups"
+    local files = vim.split(vim.fn.glob(path .. "/*.lua"), "\n")
+    for _, file in ipairs(files) do
+      local name = vim.fn.fnamemodify(file, ":t:r")
+      if name ~= "init" then
+        local ok, mod = pcall(require, "koda.groups." .. name)
+        assert.is_true(ok, "Failed to load file: " .. name)
+        assert.is_table(mod, "Module " .. name .. " did not return a table")
+      end
+    end
+  end)
+end)
+
 describe("Plugin detection logic:", function()
   local colors = palette
   local original_api = vim.pack
@@ -19,7 +34,6 @@ describe("Plugin detection logic:", function()
   it("loads only base groups when auto=true and no managers present", function()
     local config = require("koda.config")
     local opts = config.extend({ auto = true })
-
     local _, groups = Groups.setup(colors, opts)
     assert.is_true(groups["base"])
     assert.is_nil(groups["gitsigns"])
@@ -32,11 +46,9 @@ describe("Plugin detection logic:", function()
         ["telescope.nvim"] = { name = "telescope.nvim" },
       },
     }
-
     local config = require("koda.config")
     local opts = config.extend({ auto = true })
     local _, groups = Groups.setup(colors, opts)
-
     assert.is_true(groups["telescope"], "Telescope should be loaded")
     assert.is_nil(groups["blink.cmp"], "Blink should NOT be loaded")
   end)
@@ -45,7 +57,6 @@ describe("Plugin detection logic:", function()
     local config = require("koda.config")
     local opts = config.extend({ auto = false })
     local _, groups = Groups.setup(colors, opts)
-
     assert.is_true(groups["telescope"], "Telescope should be laoded")
     assert.is_true(groups["blink"], "Blink should be loaded")
   end)
