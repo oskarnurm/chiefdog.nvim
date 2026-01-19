482e1990e8ef2014f65b2c626dfc383923c21475
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-01-19 18:55:02 +0100

    feat(mason): add prettier headers

--- lua/koda/groups/mason.lua
+++ lua/koda/groups/mason.lua
@@ -1,14 +1,16 @@
 local M = {}
 
+local utils = require("koda.utils")
+
 ---@type koda.HighlightsFn
 function M.get_hl(c)
   -- stylua: ignore
   return {
     MasonNormal              = { bg = c.bg },
-    MasonHeader              = { fg = c.dim, bg = c.const, bold = true },
-    MasonHeaderSecondary     = { fg = c.dim, bg = c.info, bold = true },
+    MasonHeader              = { fg = c.highlight, bg = utils.blend(c.highlight, c.bg, 0.2), bold = true },
+    MasonHeaderSecondary     = { fg = c.const, bg = utils.blend(c.const, c.bg, 0.2), bold = true },
     MasonHighlight           = { fg = c.const },
-    MasonHighlightBlock      = { fg = c.dim, bg = c.success },
+    MasonHighlightBlock      = { fg = c.success, bg = utils.blend(c.green, c.bg, 0.2) },
     MasonHighlightBlockBold  = { fg = c.dim, bg = c.emphasis, bold = true },
     MasonMutedBlock          = { fg = c.dim, bg = c.keyword },
   }
