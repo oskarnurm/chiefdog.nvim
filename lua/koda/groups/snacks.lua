4724ca5d3664eb6e8b2ca44d274d0b553ba99a0a
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-01-19 18:27:02 +0100

    feat(plugins): add more colors to Snacks

--- lua/koda/groups/snacks.lua
+++ lua/koda/groups/snacks.lua
@@ -4,8 +4,24 @@ local M = {}
 function M.get_hl(c)
   -- stylua: ignore
   return {
-    SnacksPickerDir = { fg = c.keyword },
-    SnacksPickerMatch = { fg = c.const },
+    -- Picker
+    SnacksPickerDir            = { fg = c.keyword },
+    SnacksPickerMatch          = { fg = c.const },
+    -- Notifier
+    SnacksNotifierIconDebug    = { fg = c.comment },
+    SnacksNotifierTitleDebug   = { fg = c.comment },
+    SnacksNotifierBorderDebug  = { fg = c.comment },
+    SnacksNotifierFooterDebug  = { fg = c.comment },
+    SnacksNotifierIconInfo     = { fg = c.info },
+    SnacksNotifierTitleInfo    = { fg = c.info },
+    SnacksNotifierBorderInfo   = { fg = c.info },
+    SnacksNotifierFooterInfo   = { fg = c.info },
+    -- Input
+    SnacksInputTitle           = { fg = c.emphasis },
+    SnacksInputIcon            = { fg = c.const },
+    SnacksInputPrompt          = { fg = c.comment },
+    -- Dashboard
+    SnacksDashboardHeader     = { fg = c.fg },
   }
 end
 
