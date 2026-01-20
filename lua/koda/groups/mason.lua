d7a9b7105eb3e8e4cddea9e920d8430766426048
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-01-20 16:40:19 +0100

    fix(mason): add transparency option to MasonNormal

--- lua/koda/groups/mason.lua
+++ lua/koda/groups/mason.lua
@@ -3,10 +3,10 @@ local M = {}
 local utils = require("koda.utils")
 
 ---@type koda.HighlightsFn
-function M.get_hl(c)
+function M.get_hl(c, opts)
   -- stylua: ignore
   return {
-    MasonNormal              = { bg = c.bg },
+    MasonNormal              = { bg = opts.transparent and "none" or c.bg },
     MasonHeader              = { fg = c.highlight, bg = utils.blend(c.highlight, c.bg, 0.2), bold = true },
     MasonHeaderSecondary     = { fg = c.const, bg = utils.blend(c.const, c.bg, 0.2), bold = true },
     MasonHighlight           = { fg = c.const },
